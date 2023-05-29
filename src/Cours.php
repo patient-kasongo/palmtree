2d6835846d3a0c130f6e8fbc45aed5e1017b8c01
patient-kasongo <20ki180@esisalama.org>
2023-05-29 20:22:36 +0200

    add create cours file

---
+++ src/Cours.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App;
+
+class Cours
+{
+    private $id;
+    private $intitule;
+    private $volumeHoraire;
+    private $professeur;
+
+    /**
+     * @param mixed $intitule
+     */
+    public function setIntitule($intitule): void
+    {
+        $this->intitule = $intitule;
+    }
+
+    /**
+     * @param mixed $professeur
+     */
+    public function setProfesseur($professeur): void
+    {
+        $this->professeur = $professeur;
+    }
+
+    /**
+     * @param mixed $volumeHoraire
+     */
+    public function setVolumeHoraire($volumeHoraire): void
+    {
+        $this->volumeHoraire = $volumeHoraire;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIntitule()
+    {
+        return $this->intitule;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfesseur()
+    {
+        return $this->professeur;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVolumeHoraire()
+    {
+        return $this->volumeHoraire;
+    }
+    public function create():bool{
+        $pdo=Database::getPdo();
+        try{
+            $query="INSERT INTO cours(intitule,volumeHoraire,professeur) values(:nom, :volumeHoraire, :nomProf)";
+            $stmt=$pdo->prepare($query);
+            $stmt->execute(['nom'=>$this->intitule,
+                'volumeHoraire'=>$this->volumeHoraire,
+                'nomProf'=>$this->professeur]);
+            return true;
+        }catch (\PDOException $e){
+            return false;
+        }
+    }
+}
