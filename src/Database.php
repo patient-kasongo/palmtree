b8784d6e5fd27b21f7fd081f18805dd8d17722db
walter <20km196@esisalama.org>
2023-05-29 09:54:52 +0200

    la class Database

---
+++ src/Database.php
@@ -0,0 +1,19 @@
+<?php
+    class Database
+    {
+        public static $pdo;
+
+        public static function getPdo()
+        {
+            if(!self::$pdo){
+                try {
+                    self::$pdo = new PDO("mysql:host:localhost;dbname=palmts",'root','',[
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+                    ]);
+                } catch (PDOException $pdo) {
+                    echo "Erreur de connection à la base des données!";
+                }
+            }
+        }
+    }
